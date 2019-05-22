b383a2c158e9a390e9b3c8a8cf86fc8ec1d84a90
Paul Chan <paul.chan@rakuten.com>
2019-05-22 23:14:31 +0900

    Initial Commit

---
+++ RubyConverterSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RubyConverterSwift
+//
+//  Created by Chan, Paul | Paul | PAYSD on 2019/05/22.
+//  Copyright © 2019年 Chan Ka Ho, Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
